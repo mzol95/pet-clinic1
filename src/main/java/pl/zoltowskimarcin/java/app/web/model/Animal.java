03627d90e1c4c063c7fade18017b48c36a295790
marcin <m.s.zoltowski@gmail.com>
2024-01-20 15:48:44 +0100

    feat: adding classes bodies

--- src/main/java/pl/zoltowskimarcin/java/app/web/model/Animal.java
+++ src/main/java/pl/zoltowskimarcin/java/app/web/model/Animal.java
@@ -1,7 +1,44 @@
 package pl.zoltowskimarcin.java.app.web.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Animal {
 
     private String name;
-    private int age;
+    private LocalDate birthDate;
+    private List<Visit> visitList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Visit> getVisitList() {
+        return visitList;
+    }
+
+    public void setVisitList(List<Visit> visitList) {
+        this.visitList = visitList;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", visitList=" + visitList +
+                '}';
+    }
 }
--- src/main/java/pl/zoltowskimarcin/java/app/web/model/Clinic.java
+++ src/main/java/pl/zoltowskimarcin/java/app/web/model/Clinic.java
@@ -1,4 +1,29 @@
 package pl.zoltowskimarcin.java.app.web.model;
 
+import java.util.Set;
+
 public class Clinic {
+
+    private Set<Doctor> doctorSet;
+
+    public Clinic() {
+    }
+
+    public Clinic(Set<Doctor> doctorSet) {
+        this.doctorSet = doctorSet;
+    }
+
+    public Set<Doctor> getDoctorSet() {
+        return doctorSet;
+    }
+
+    public void setDoctorSet(Set<Doctor> doctorSet) {
+        this.doctorSet = doctorSet;
+    }
+
+    @Override
+    public String toString() {
+        return "Clinic{" + "doctorSet=" + doctorSet + '}';
+    }
 }
+
