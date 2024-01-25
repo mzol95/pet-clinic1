175d8c7c6d0511be7dea543fbefd43e016f86375
marcin <m.s.zoltowski@gmail.com>
2024-01-25 17:50:57 +0100

    feat: added homework

--- src/main/java/pl/zoltowskimarcin/java/app/repository/AnimalRepository.java
+++ src/main/java/pl/zoltowskimarcin/java/app/repository/AnimalRepository.java
@@ -10,6 +10,14 @@ public class AnimalRepository {
 
     public Animal create(Animal animal) {
         LOGGER.info("create(" + animal + ")");
+//todo 25.01.2024
+//zaimplementować dostęp do bazy danych za pomocą JDBC dla klasy Animal i metody create -> stworzyć tabele w bazi
+//kroki do wykonania dla JDBC
+        //1. DriverManager
+        //2. Connection
+        //3. Statement/PreparedStatement
+        //4. ResultSet
+        //0. try-with-resources
 
         LOGGER.info("create(...) = ");
         return null;
