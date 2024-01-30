5a3344b24ccd59c4fc73cd959fb7478788b2ec7a
marcin <m.s.zoltowski@gmail.com>
2024-01-30 19:28:52 +0100

    feat: add persistance layer classes

---
+++ src/main/java/pl/zoltowskimarcin/java/app/repository/AnimalDao.java
@@ -0,0 +1,16 @@
+package pl.zoltowskimarcin.java.app.repository;
+
+import pl.zoltowskimarcin.java.app.web.model.Animal;
+
+import java.util.Optional;
+
+public interface AnimalDao {
+
+    Animal create(Animal animal);
+
+    Optional<Animal> read(Long id);
+
+    Animal update(Animal animal);
+
+    boolean delete(Long id);
+}
